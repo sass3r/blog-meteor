2b9f3518da8e4a12f17e200dbfb6a1f5da64e6e9
Rafael Ramirez <mrsblast@gmail.com>
2016-05-26 16:53:23 -0400

    Last changes.

--- client/main.js
+++ client/main.js
@@ -3,20 +3,24 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+Template.body.events({
+	submit(evt,tpl){
+		evt.preventDefault();
+		console.log(tpl.find('[name=title]'));
+		console.log("this should show later create post");
+	}
+
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
+Template.body.helpers({
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+	posts(){
+
+		return [{
+			title:'Post1',
+			text:'Content'},
+			{title:'Post2',
+			text:'Content2'
+		}];
+	}
 });
